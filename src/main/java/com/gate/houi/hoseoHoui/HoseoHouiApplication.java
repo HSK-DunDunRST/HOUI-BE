fee9896089d585e713a15f3144edae0c97f45446
HSK-DunDunRST <gon5073@naver.com>
2025-06-12 19:17:15 +0900

    Project Created

---
+++ src/main/java/com/gate/houi/hoseoHoui/HoseoHouiApplication.java
@@ -0,0 +1,13 @@
+package com.gate.houi.hoseoHoui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoseoHouiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoseoHouiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gate/houi/hoseoHoui/HoseoHouiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gate.houi.hoseoHoui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoseoHouiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
