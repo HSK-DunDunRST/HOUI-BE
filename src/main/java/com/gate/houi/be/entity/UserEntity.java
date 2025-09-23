683df8777364c9afb61e7659489b6a0cb5a219b4
HSK-DunDunRST <gon5073@naver.com>
2025-09-23 09:31:57 +0900

    :sparkles: FEAT - Redis + JWT + Security 추가 및 기본 설정

--- src/main/java/com/gate/houi/be/entity/UserEntity.java
+++ src/main/java/com/gate/houi/be/entity/UserEntity.java
@@ -8,11 +8,9 @@ import lombok.*;
 import java.util.UUID;
 
 import org.hibernate.annotations.UuidGenerator;
-
 @Entity
-@Table(name = "student_data")
-@Getter
-@Setter
+@Table(name = "users")
+@Getter @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,20 +20,21 @@ public class UserEntity extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // UUID v4 (랜덤 UUID) 사용 - Hibernate 최신 방식
     @UuidGenerator
     @Column(name = "user_uuid", nullable = false, unique = true, updatable = false, columnDefinition = "BINARY(16)")
     private UUID userUuid;
 
-    @Column(name = "student_id", nullable = false, length = 9)
-    private String studentId;
+    @Column(name = "student_id", unique = true, length = 9)
+    private String studentId; // 학생만 사용
 
     @Column(name = "user_name", nullable = false, length = 50)
     private String userName;
 
+    private String loginPwd; // 관리자만 사용
+
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private Provider oauth_provider;
+    private Provider oauthProvider;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
