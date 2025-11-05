59ed7120f09b2a4b045bf11a48d51cbb4e9cd3e5
HSK-DunDunRST <gon5073@naver.com>
2025-11-05 23:34:35 +0900

    Create Project

---
+++ src/main/java/com/gate/houi/GoogleBackEnd/GoogleBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.gate.houi.GoogleBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleBackEndApplication.class, args);
+	}
+
+}
